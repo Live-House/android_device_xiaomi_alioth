81ba9f2f048d09661719de8be088d721052b489e
bxySo <bxyso@outlook.com>
2024-12-17 16:00:01 +0800

    alioth: overlay: Import regulatory info for M2012K11AC

--- device.mk
+++ device.mk
@@ -39,6 +39,7 @@ TARGET_BOARD_PLATFORM := kona
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
+    SettingsOverlayM2012K11AC \
     SettingsOverlayM2012K11AG \
     SettingsOverlayM2012K11AI \
     SettingsProviderM2012K11AC \
