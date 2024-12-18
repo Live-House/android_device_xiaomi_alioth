05aa448c5f9a64aaea664f12bfc7ee8a69ef980b
bxySo <bxyso@outlook.com>
2024-12-19 01:10:58 +0800

    alioth: overlay: Rework and Cleanup

--- device.mk
+++ device.mk
@@ -37,13 +37,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 # RRO Overlays
 PRODUCT_PACKAGES += \
     SettingsOverlayM2012K11AC \
-    SettingsOverlayM2012K11AG \
     SettingsOverlayM2012K11AI \
     SettingsProviderM2012K11AC \
-    SettingsProviderM2012K11AG \
     SettingsProviderM2012K11AI \
     WifiOverlayM2012K11AC \
-    WifiOverlayM2012K11AG \
     WifiOverlayM2012K11AI
 
 # Shipping API level
