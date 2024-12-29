3b2b02d2772ff8e4d248e1d378e1571bb498fd43
bxySo <bxyso@outlook.com>
2024-12-29 10:25:59 +0800

    Partial Revert "alioth: overlay: Rework and Cleanup"
    `SRO Overlay` didn't work,
    so switch back to `RRO Overlay` for `WifiOverlayM2012K11AG`
    
    This reverts commit 05aa448c5f9a64aaea664f12bfc7ee8a69ef980b.

--- device.mk
+++ device.mk
@@ -44,6 +44,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderM2012K11AC \
     SettingsProviderM2012K11AI \
     WifiOverlayM2012K11AC \
+    WifiOverlayM2012K11AG \
     WifiOverlayM2012K11AI
 
 # Shipping API level
