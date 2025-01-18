f3145b8ec6feedac24cd479aa52f5c620e8b3e60
bxySo <bxyso@outlook.com>
2025-01-18 13:56:56 +0800

    alioth: Build DSPVolumeSynchronizer
    * Follow up: https://github.com/Live-House/android_hardware_xiaomi/commit/9edc4630ffca809278726793f0a9ea82590675a5

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# DSP Volume Synchronizer
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
