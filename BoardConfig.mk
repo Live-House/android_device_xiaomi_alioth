81c3aa60986eb9653e490bb2f58d5d35130ed9d7
bxySo <bxyso@outlook.com>
2025-01-25 13:36:31 +0800

    Revert "alioth: Inherit from vendor/xiaomi/alioth-firmware"
    This reverts commit 21e430eda3c6f32fc2e4c6defeedc9d2e3db721c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,3 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
 include vendor/xiaomi/alioth/BoardConfigVendor.mk
-
-# Inherit from alioth-firmware
-include vendor/xiaomi/alioth-firmware/BoardConfigVendor.mk
